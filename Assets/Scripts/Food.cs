6d8a11e06264194d55076ada1c1e4db22a3bc8af
AnOkayGitHub <71465647+AnOkayGitHub@users.noreply.github.com>
2021-11-02 16:15:03 -0400

    Test

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+<<<<<<< Updated upstream
 /*test2*/
 
+=======
+>>>>>>> Stashed changes
 public class Food : MonoBehaviour
 {
     [SerializeField] private BoxCollider2D grid;
